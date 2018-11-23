b01d38176a536574abb784b179fab6ea78017629
Robert <treboryx@gmail.com>
2018-11-24 00:15:48 +0100

    small change

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,17 @@ window.addEventListener('scroll', function() {
   if (scrollPosition >= 100) logoContainer.classList.add('arrow--scrolled');
   else logoContainer.classList.remove('arrow--scrolled');
 });
+
+var text = ["<| Robert |>", "<| rowboat |>", "<| treboryx |>"];
+var counter = 0;
+var elem = document.getElementById("aliases");
+var inst = setInterval(change, 1000);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
